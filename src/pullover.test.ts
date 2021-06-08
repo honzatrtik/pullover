afa3a3ba1b090cb68e140c99a3fa16af7b523c0f
honzatrtik <honza.trtik@gmail.com>
2021-06-09 01:06:15 +0200

    Switch tabs for spaces

--- src/pullover.test.ts
+++ src/pullover.test.ts
@@ -7,106 +7,106 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 describe('pullover', () => {
-	describe('of', () => {
-		it('should create stream with one item', async () => {
-			expect(await runToArray(of(666))).to.eql([666]);
-		});
-	});
+    describe('of', () => {
+        it('should create stream with one item', async () => {
+            expect(await runToArray(of(666))).to.eql([666]);
+        });
+    });
 
-	describe('delay', () => {
-		it('should emit one item to stream with delay', async () => {
-			const result1 = await Promise.race([
-				runToArray(empty),
-				runToArray(delay(10, 10)),
-			]);
+    describe('delay', () => {
+        it('should emit one item to stream with delay', async () => {
+            const result1 = await Promise.race([
+                runToArray(empty),
+                runToArray(delay(10, 10)),
+            ]);
 
-			const result2 = await Promise.race([
-				runToArray(delay(10, 10)),
-				runToArray(delay(11, 11)),
-			]);
+            const result2 = await Promise.race([
+                runToArray(delay(10, 10)),
+                runToArray(delay(11, 11)),
+            ]);
 
-			const result3 = await Promise.race([
-				runToArray(delay(9, 9)),
-				runToArray(delay(10, 10)),
-			]);
+            const result3 = await Promise.race([
+                runToArray(delay(9, 9)),
+                runToArray(delay(10, 10)),
+            ]);
 
-			expect(result1).to.eql([]);
-			expect(result2).to.eql([10]);
-			expect(result3).to.eql([9]);
-		});
-	});
+            expect(result1).to.eql([]);
+            expect(result2).to.eql([10]);
+            expect(result3).to.eql([9]);
+        });
+    });
 
-	describe('empty', () => {
-		it('should create empty stream', async () => {
-			expect(await runToArray(empty)).to.eql([]);
-		});
-	});
+    describe('empty', () => {
+        it('should create empty stream', async () => {
+            expect(await runToArray(empty)).to.eql([]);
+        });
+    });
 
-	describe('concat', () => {
-		it('should concatenate two streams', async () => {
-			const stream = pipe(
-				of(1),
-				concat(of(2)),
-			)
-			expect(await runToArray(stream)).to.eql([1, 2]);
-		});
-		it('should return empty stream if concatenating two empty streams', async () => {
-			expect(await runToArray(concat(empty)(empty))).to.eql([]);
-		});
-	});
+    describe('concat', () => {
+        it('should concatenate two streams', async () => {
+            const stream = pipe(
+                of(1),
+                concat(of(2)),
+            )
+            expect(await runToArray(stream)).to.eql([1, 2]);
+        });
+        it('should return empty stream if concatenating two empty streams', async () => {
+            expect(await runToArray(concat(empty)(empty))).to.eql([]);
+        });
+    });
 
-	describe('throwError', () => {
-		it('should create failed stream', async () => {
-			const stream = throwError('Some error');
-			expect(run(stream)).to.be.rejectedWith('Some error')
-		});
-	});
+    describe('throwError', () => {
+        it('should create failed stream', async () => {
+            const stream = throwError('Some error');
+            expect(run(stream)).to.be.rejectedWith('Some error')
+        });
+    });
 
-	describe('recover', () => {
-		it('should recover from failed stream with passed stream', async () => {
-			const failedStream = throwError('Some error');
-			const streamWithRecovery = pipe(
-				failedStream,
-				recover(() => of(666))
-			);
-			expect(await runToArray(streamWithRecovery)).to.eql([666]);
-		});
-	});
+    describe('recover', () => {
+        it('should recover from failed stream with passed stream', async () => {
+            const failedStream = throwError('Some error');
+            const streamWithRecovery = pipe(
+                failedStream,
+                recover(() => of(666))
+            );
+            expect(await runToArray(streamWithRecovery)).to.eql([666]);
+        });
+    });
 
-	describe('fromIterable', () => {
-		it('should create stream from any iterable', async () => {
-			const stream = fromIterable({
-				[Symbol.iterator]: function* () {
-					yield 1;
-					yield 2;
-				},
-			});
+    describe('fromIterable', () => {
+        it('should create stream from any iterable', async () => {
+            const stream = fromIterable({
+                [Symbol.iterator]: function* () {
+                    yield 1;
+                    yield 2;
+                },
+            });
 
-			const asyncStream = fromIterable({
-				[Symbol.asyncIterator]: async function* () {
-					yield Promise.resolve(3);
-					yield Promise.resolve(4);
-				},
-			})
+            const asyncStream = fromIterable({
+                [Symbol.asyncIterator]: async function* () {
+                    yield Promise.resolve(3);
+                    yield Promise.resolve(4);
+                },
+            })
 
-			expect(await runToArray(stream)).to.eql([1,2]);
-			expect(await runToArray(asyncStream)).to.eql([3,4]);
-		});
-	});
+            expect(await runToArray(stream)).to.eql([1,2]);
+            expect(await runToArray(asyncStream)).to.eql([3,4]);
+        });
+    });
 
-	describe('mapAsync', () => {
-		it('should keep order of items in stream', async () => {
-			const stream = pipe(
-				fromIterable([0,1,2,3,4,5,6,7,8,9]),
-				mapAsync(n =>
-					new Promise(
-					resolve => setTimeout(() => resolve(n * 10), 10 - n)
-					),
-					5
-				)
-			);
+    describe('mapAsync', () => {
+        it('should keep order of items in stream', async () => {
+            const stream = pipe(
+                fromIterable([0,1,2,3,4,5,6,7,8,9]),
+                mapAsync(n =>
+                    new Promise(
+                    resolve => setTimeout(() => resolve(n * 10), 10 - n)
+                    ),
+                    5
+                )
+            );
 
-			expect(await runToArray(stream)).to.eql([0,10,20,30,40,50,60,70,80,90]);
-		});
-	});
+            expect(await runToArray(stream)).to.eql([0,10,20,30,40,50,60,70,80,90]);
+        });
+    });
 });
--- src/semaphore.ts
+++ src/semaphore.ts
@@ -5,39 +5,39 @@ type AcquireFunction = () => Promise<ReleaseFunction>;
 type ReleaseFunction = () => void;
 
 export interface Semaphore {
-	acquire: AcquireFunction,
-	available: () => number,
+    acquire: AcquireFunction,
+    available: () => number,
 }
 
 export const makeSemaphore = (max: number): Semaphore => {
-	const locks = new Set<Symbol>();
-	const eventEmitter = new EventEmitter();
+    const locks = new Set<Symbol>();
+    const eventEmitter = new EventEmitter();
 
-	const makeRelease = (): ReleaseFunction => {
-		const lock = Symbol();
-		locks.add(lock);
-		const keepAliveInterval = setInterval(() => console.log('tick'), 1000);
-		const release = () => {
-			if (locks.has(lock)) {
-				locks.delete(lock);
-				clearInterval(keepAliveInterval);
-				eventEmitter.emit('released');
-			}
-		}
+    const makeRelease = (): ReleaseFunction => {
+        const lock = Symbol();
+        locks.add(lock);
+        const keepAliveInterval = setInterval(() => console.log('tick'), 1000);
+        const release = () => {
+            if (locks.has(lock)) {
+                locks.delete(lock);
+                clearInterval(keepAliveInterval);
+                eventEmitter.emit('released');
+            }
+        }
 
-		return release;
-	}
+        return release;
+    }
 
-	const acquire = (): Promise<ReleaseFunction> =>
-		locks.size < max
-			? Promise.resolve(makeRelease())
-			: new Promise(
-				resolve => eventEmitter.once('released', () => resolve(makeRelease()))
-			);
+    const acquire = (): Promise<ReleaseFunction> =>
+        locks.size < max
+            ? Promise.resolve(makeRelease())
+            : new Promise(
+                resolve => eventEmitter.once('released', () => resolve(makeRelease()))
+            );
 
-	return {
-		acquire,
-		available: () => max - locks.size,
-	};
+    return {
+        acquire,
+        available: () => max - locks.size,
+    };
 };
 
